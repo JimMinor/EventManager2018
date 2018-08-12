f108918cd0aecc585fc0c6fd8e877731364dc131
Mcarlo92 <33984528+Mcarlo92@users.noreply.github.com>
2018-08-12 16:52:14 +0200

    inizio progetto e il sito per scaricare scene builder

---
+++ ticket/src/tiket/FXMLDocumentController.java
@@ -0,0 +1,29 @@
+
+package tiket;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private Button ENTRA1;
+    @FXML
+    private Label label;
+
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TUDU
+    }    
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+    }
+    
+}
---
+++ ticket/src/tiket/M1Controller.java
@@ -0,0 +1,22 @@
+
+package tiket;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+
+public class M1Controller implements Initializable {
+
+    @FXML
+    private AnchorPane AnchorPane;
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TUDU
+    }    
+    
+}
