4885d2a253777bda987e5000b263a35f0318c733
GiaMoscariello <gia.moscariello@gmail.com>
2018-08-25 11:35:48 +0200

    Modifica procedura in funzione

--- DB/procedure.sql
+++ DB/procedure.sql
@@ -1,16 +1,20 @@
-CREATE PROCEDURE ins_evento_musicale (new_nome varchar2, new_luogo varchar2, new_data date, new_prezzo float, new_descrizione varchar2, new_tipologia varchar2, new_artista)
+create or replace function ins_evento_musicale (new_nome varchar2, new_luogo varchar2, new_data date, new_prezzo float, new_descrizione varchar2, new_tipologia varchar2, new_artista varchar2)
 
-@new_id number
+return NUMBER is new_id NUMBER;
 
 BEGIN
     INSERT INTO Evento(ID, nome, luogo, data, prezzo, descrizione, tipologia)
         VALUES('0', new_nome, new_luogo, new_data, new_prezzo, new_descrizione, new_tipologia);
-        
-    SELECT @new_id=id 
+
+        SELECT id into new_id 
         FROM Evento
-        WHERE nome=new_nome, luogo=new_luogo, data=new_data, prezzo=new_prezzo, descrizione=new_descrizione, tipologia=new_tipologia;
-        
+        WHERE nome=new_nome and luogo=new_luogo and data=new_data and prezzo=new_prezzo
+        and descrizione=new_descrizione and tipologia=new_tipologia;
+
     INSERT INTO Evento_Musicale(ID, artista)
-        VALUES(@new_id, new_artista);
+        VALUES(new_id, new_artista);
         
-END ins_evento_sportivo;
+        
+        RETURN new_id;
+
+END ins_evento_musicale;
