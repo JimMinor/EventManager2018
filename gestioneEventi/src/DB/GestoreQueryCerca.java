682e37d4603c53223904caf2d50fd5f63b498a18
Mcarlo92 <33984528+Mcarlo92@users.noreply.github.com>
2018-09-25 15:57:00 +0200

    query

--- gestioneEventi/src/DB/GestoreQueryCerca.java
+++ gestioneEventi/src/DB/GestoreQueryCerca.java
@@ -10,6 +10,76 @@ import java.text.SimpleDateFormat;
 
 public class GestoreQueryCerca {//query generica per cercare tutti gli elementi appena apre la schermata
 
+    public ResultSet cercaCliente(String username){
+        try {
+            Connection connection = UtilityDB.getConnessioneDB();
+            // Create tutti i risultati in una tabella
+            String selectSql="";
+            //{0}
+            if (username==null) {
+                selectSql = "SELECT USERNAME, NOME, COGNOME FROM CLIENTE NATURAL JOIN PERSONA ;";
+            }
+            //{1}
+            else  selectSql = "SELECT USERNAME, NOME, COGNOME FROM CLIENTE NATURAL JOIN PERSONA WHERE USERNAME="+ username+";";
+            try (Statement statement = connection.createStatement();
+                 ResultSet rS = statement.executeQuery(selectSql)) {
+
+                connection.close();
+                return rS;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    public ResultSet cercaImpiegato (String nome, String cognome , Date datanascita){
+        try {
+            Connection connection = UtilityDB.getConnessioneDB();
+            // Create tutti i risultati in una tabella
+            String selectSql="SELECT NOME , COGNOME , CODICE_FISCALE FROM IMPIGATO NATURAL JOIN PERSONA";
+            //{1,2,3}
+            if (nome!=null && cognome!=null && datanascita!=null) {
+                Format formatter = new SimpleDateFormat("dd-MM-yy");
+                String dataRicerca = formatter.format(datanascita);
+                selectSql +=" WHERE " + "NOME" + "=" + nome + " AND "+ "COGNOME" + "=" + cognome + " AND DATA=TO_DATE('" + dataRicerca + "','dd-MM-yy');";
+            }
+            //{1,2}
+            else if (nome!=null && cognome!=null && datanascita=null) {
+
+                selectSql +=" WHERE " + "NOME" + "=" + nome + " AND "+ "COGNOME" + "=" + cognome + ";";
+            }
+            //{1,3}
+            else if (nome!=null && cognome=null && datanascita!=null) {
+                Format formatter = new SimpleDateFormat("dd-MM-yy");
+                String dataRicerca = formatter.format(datanascita);
+                selectSql +=" WHERE " + "NOME" + "=" + nome + " AND DATA=TO_DATE('" + dataRicerca + "','dd-MM-yy');";
+            }
+            //{2,3}
+            else if (nome=null && cognome!=null && datanascita!=null) {
+                Format formatter = new SimpleDateFormat("dd-MM-yy");
+                String dataRicerca = formatter.format(datanascita);
+                selectSql +=" WHERE "+ "COGNOME" + "=" + cognome + " AND DATA=TO_DATE('" + dataRicerca + "','dd-MM-yy');";
+            }
+            //{1}
+            else if (nome!=null && cognome=null && datanascita=null) {
+
+                selectSql +=" WHERE " + "NOME" + "=" + nome +" ;";
+            }
+            //{2}
+            else if (nome=null && cognome!=null && datanascita=null) {
+
+                selectSql +=" WHERE " + "COGNOME" + "=" + cognome + ";";
+            }
+            //{3}
+            else if (nome!=null && cognome!=null && datanascita!=null) {
+                Format formatter = new SimpleDateFormat("dd-MM-yy");
+                String dataRicerca = formatter.format(datanascita);
+                selectSql +=" WHERE " + "NOME" + "=" + nome + " AND "+ "COGNOME" + "=" + cognome + " AND DATA=TO_DATE('" + dataRicerca + "','dd-MM-yy');";
+            }
+
+    }
 
     public ResultSet cerca(String tabella,String colonnaTW1 ,String colonnaTW2 , String colonnaTW3, String attributo1 ,String attributo2 , Date attributo3 ) throws SQLException{
 
