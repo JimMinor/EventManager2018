a49e18c7d05aeb3a370f26da08ed563782e81752
Ghon0 <vittorio.iannaco.93@gmail.com>
2018-09-25 18:41:33 +0200

    fix

--- gestioneEventi/src/ControllerView/VisualizzaClientiControllerView.java
+++ gestioneEventi/src/ControllerView/VisualizzaClientiControllerView.java
@@ -11,14 +11,7 @@ import javafx.scene.layout.AnchorPane;
 
 public class VisualizzaClientiControllerView {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-
-=======
->>>>>>> c6d38f69abc6b0b77205b1836aaeea18aa959245
-=======
->>>>>>> c6d38f69abc6b0b77205b1836aaeea18aa959245
    @FXML
     private TextField usernameClienteTextField;
   @FXML
@@ -92,31 +85,8 @@ public class VisualizzaClientiControllerView {
     public JFXTextField getSpesaTotaleTextField() {
         return spesaTotaleTextField;
     }
+    
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    @FXML
-   private TextField indirizzoClienteTextField;
-   @FXML
-   private TextField emailClienteTextField;
-   @FXML
-   private RadioButton sessoFClienteRadioButton;
-   @FXML
-   private RadioButton sessoMClienteRadioButton;
-   @FXML
-   private Button fineClientiButton;
-   @FXML
-   private AnchorPane visualizzaClientiForm;
-   @FXML
-   private JFXTextField bigliettiAcquistatiTextField;
-   @FXML
-   private JFXTextField spesaTotaleTextField;
-
-=======
->>>>>>> c6d38f69abc6b0b77205b1836aaeea18aa959245
-
-=======
->>>>>>> c6d38f69abc6b0b77205b1836aaeea18aa959245
 
     public void sessoFClienteRadioButtonPressed(ActionEvent actionEvent) {
         sessoMClienteRadioButton.setSelected(false);
